f9aaad9278ccfd20d961e0431dd9a27e2e921641
devopsprosiva <devopsprosiva@gmail.com>
2023-03-07 12:58:07 -0800

    Update outputs.tf

--- deployment-us-east-1/outputs.tf
+++ deployment-us-east-1/outputs.tf
@@ -9,3 +9,7 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = module.deployment-us-east-1.private_subnet_ids
 }
+
+output "vpc_cidr" {
+  value = module.deployment-us-east-1.vpc_cidr
+}
